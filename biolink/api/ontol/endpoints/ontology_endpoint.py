d575605ed75d71a93c2cd4bba96d247898eab731
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-10-24 14:40:35 -0700

    fixing the other category of the ribbon summary

--- biolink/api/ontol/endpoints/ontology_endpoint.py
+++ biolink/api/ontol/endpoints/ontology_endpoint.py
@@ -307,6 +307,13 @@ class OntologyRibbons(Resource):
         "C": "GO:0005575"
     }
 
+    def get_category_terms(self, category):
+        terms = []
+        for group in category["groups"]:
+            if group["type"] == "Term":
+                terms.append(group)
+        return terms
+
     @api.expect(ribbon_parser)
     def get(self):
         """
@@ -322,8 +329,6 @@ class OntologyRibbons(Resource):
         # Step 1: create the categories
         categories = OntologySubset.get(self, args.subset)
         for category in categories:
-            # category["tooltip_class_label"] = ["class", "classes"]
-            # category["tooltip_annotation_label"] = ["annotation", "annotations"]
 
             category["groups"] = category["terms"]
             del category["terms"]
@@ -343,11 +348,10 @@ class OntologyRibbons(Resource):
 
                 group["type"] = "Term"
             
-            category["groups"] = [{"id" : category["id"], "label" : "all " + category["label"].lower().replace("_", " "), "description" : "Represent all annotations", "type" : "All"}] + category["groups"] + [{"id" : category["id"], "label" : "other " + category["label"].lower().replace("_", " "), "description" : "Represent all annotations not mapped to a specific category", "type" : "Other"}]
+            category["groups"] = [{"id" : category["id"], "label" : "all " + category["label"].lower().replace("_", " "), "description" : "Show all " + category["label"].lower().replace("_", " ") + " annotations", "type" : "All"}] + category["groups"] + [{"id" : category["id"], "label" : "other " + category["label"].lower().replace("_", " "), "description" : "Represent all annotations not mapped to a specific term", "type" : "Other"}]
         
         # Step 2: create the entities / subjects
         subject_ids = args.subject
-        # print("SUBS : " , subject_ids)
 
         # ID conversion
         subject_ids = [x.replace('WormBase:', 'WB:') if 'WormBase:' in x else x for x in subject_ids]
@@ -462,37 +466,62 @@ class OntologyRibbons(Resource):
                                 # entity['nb_annotations'] += 1
 
 
+                # other = get_others()
+                terms = self.get_category_terms(cat)
+                terms = [term["id"] for term in terms]
+                
                 other = { }
                 other["ALL"] = { "terms" : set(), "nb_classes" : 0, "nb_annotations" : 0 }
 
-                # creating list of annotations not annotated to a specific term
-                not_found = set()
                 for annot in data:
-                    found = False
-                    for gp in cat['groups']:
-                        if annot['annotation_class'] == gp['id']:
-                            found = True
-                    if not found:    
-                        not_found.add(annot['annotation_class'])
+                    aspect = self.aspect_map[annot["aspect"]]
 
-                for nf in not_found:
-                    for annot in data:
-                        if nf == annot['annotation_class']:
-                            # print(nf, annot['annotation_class'])
-                            aspect = self.aspect_map[annot["aspect"]]
+                    # only allow annotated terms belonging to the same category if cross_aspect
+                    if cross_aspect or cat['id'] == aspect:
 
-                            # only allow annotated terms belonging to the same category
-                            if cat['id'] == aspect:
+                        found = False
+                        for term in terms:
+                            if term in annot["regulates_closure"]:
+                                found = True
+                                break
+
+                        if not found:
+                            other["ALL"]["nb_annotations"] += 1
+                            other["ALL"]["terms"].add(annot['annotation_class'])
+                            if annot['evidence_type'] not in other:
+                                other[annot['evidence_type']] = { "terms" : set(), "nb_classes" : 0, "nb_annotations" : 0 }
+                            other[annot['evidence_type']]["nb_annotations"] += 1
+                            other[annot['evidence_type']]["terms"].add(annot['annotation_class'])
+                
+
+                # # creating list of annotations not annotated to a specific term
+                # not_found = set()
+                # for annot in data:
+                #     found = False
+                #     for gp in cat['groups']:
+                #         if annot['annotation_class'] == gp['id']:
+                #             found = True
+                #     if not found:    
+                #         not_found.add(annot['annotation_class'])
+
+                # for nf in not_found:
+                #     for annot in data:
+                #         if nf == annot['annotation_class']:
+                #             # print(nf, annot['annotation_class'])
+                #             aspect = self.aspect_map[annot["aspect"]]
+
+                #             # only allow annotated terms belonging to the same category
+                #             if cat['id'] == aspect:
                         
-                                # if the subgroup has not been met yet, create it
-                                if annot['evidence_type'] not in other:
-                                    other[annot['evidence_type']] = { "terms" : set(), "nb_classes" : 0, "nb_annotations" : 0 }
+                #                 # if the subgroup has not been met yet, create it
+                #                 if annot['evidence_type'] not in other:
+                #                     other[annot['evidence_type']] = { "terms" : set(), "nb_classes" : 0, "nb_annotations" : 0 }
 
-                                # for each annotation, add the term and increment the nb of annotations
-                                other[annot['evidence_type']]['terms'].add(annot['annotation_class'])
-                                other[annot['evidence_type']]['nb_annotations'] += 1
-                                other['ALL']['terms'].add(annot['annotation_class'])
-                                other['ALL']['nb_annotations'] += 1
+                #                 # for each annotation, add the term and increment the nb of annotations
+                #                 other[annot['evidence_type']]['terms'].add(annot['annotation_class'])
+                #                 other[annot['evidence_type']]['nb_annotations'] += 1
+                #                 other['ALL']['terms'].add(annot['annotation_class'])
+                #                 other['ALL']['nb_annotations'] += 1
                     
                 entity['groups'][cat['id'] + "-other"] = other
 
@@ -510,7 +539,8 @@ class OntologyRibbons(Resource):
 
             subjects.append(entity)
 
-        # lastly, fill out the entity details
+
+        # fill out the entity details
         q = "*:*"
         qf = ""
         fq = "&fq=bioentity:(\"" + "\" or \"".join(mod_ids) + "\")&rows=100000"
@@ -525,6 +555,7 @@ class OntologyRibbons(Resource):
                     entity['label'] = entity_detail['bioentity_label']
                     entity['taxon_id'] = entity_detail['taxon']
                     entity['taxon_label'] = entity_detail['taxon_label']
+
 
         # map the entity back to their original IDs
         for entity in subjects:
